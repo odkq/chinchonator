0344c86464c6eee05ffe612ed351aa06c0886675
Pablo Martín <pmartin@assia-inc.com>
2019-08-18 14:28:12 +0200

    Evaluation and movement

--- chinchonator.py
+++ chinchonator.py
@@ -4,23 +4,33 @@ Copyright (C) 2019 Pablo Martín <pablo@odkq.com>
 This is distributed under the GPL license, see LICENSE file for details
 """
 
+import re
 import cmd
-import random
 import sys
+import copy
+import random
+import itertools
 
 
 def colorize(inputstr, colornum):
     ''' Use ANSI scape sequences to colorize string '''
     return "\033[1;{};40m{}\033[0;37;40m".format(colornum, inputstr)
 
+
 def pinfo(inputstr):
     print(colorize(' *** ' + inputstr, 36))
+
 
 def pdebug(inputstr):
     print(colorize(inputstr, 30))
 
+
 def perror(inputstr):
     print(colorize('ERROR: ' + inputstr, 31))
+
+
+class ParseError(Exception):
+    pass
 
 
 class Card:
@@ -29,24 +39,55 @@ class Card:
      with 4 suits ('Bastos', 'Copas', 'Espadas', 'Oros')
      and 10 cards each (from 1 to 7 and 3 'figuras':
       10 (Sota), 11 (Caballo), 12 (Rey)
+    Internally Sota, Caballo and Rey will be 8, 9, 10 to
+    ease calculations, but shown their traditional numbers
+    on representation (__repr__)
     """
     suit_color = {
             "Bastos": 32,
             "Espadas": 34,
             "Copas": 35,
-            "Oros": 33 }
+            "Oros": 33}
 
     def __init__(self, suit, number):
         self.suit = suit
         self.number = number
+
+        # XXX: Check this when playing with a 52 cards deck
         if (self.number > 10):
             self.value = 10
         else:
             self.value = self.number
 
+        if self.number > 12:
+            raise ParseError('Number {} too high'.format(self.number))
+
     def __repr__(self):
-        return colorize('{}{}'.format(self.number, self.suit[0]),
+        number = self.number
+        if number > 7:
+            number = self.number + 2
+        return colorize('{}{}'.format(number, self.suit[0]),
                         Card.suit_color[self.suit])
+
+
+class ParsedCard(Card):
+    """ Initialize a card object from a string like '10C' """
+    def __init__(self, cardstr):
+        splitted = re.split('(\d+)', cardstr)
+        if len(splitted) != 3:
+            raise ParseError('Unable to parse card "{}"'.format(cardstr))
+        try:
+            number = int(splitted[1])
+        except ValueError:
+            raise ParseError('Unable to parse card "{}"'.format(cardstr))
+        suit = None
+        for key in Card.suit_color.keys():
+            if key[0] == splitted[2]:
+                suit = key
+                break
+        if suit is None:
+            raise ParseError('Unable to parse card "{}"'.format(cardstr))
+        Card.__init__(self, suit, number)
 
 
 class Deck:
@@ -90,7 +131,10 @@ class Deck:
         return repstr
 
     def last(self):
-        return self.table[len(self.table) - 1]
+        try:
+            return self.table[len(self.table) - 1]
+        except IndexError:
+            return 'Empty'
 
 
 class IllegalMovement(Exception):
@@ -99,9 +143,9 @@ class IllegalMovement(Exception):
 
 class Hand:
     '''
-    The player (hand) has 7 cards. He starts its turn by getting a card, either
-    face up (from the deck) or up (from the table). After that he puts a card
-    on top of the table (deck.put())
+    The player (hand) has 7 cards. He starts its turn by getting a card,
+    either face up (from the deck) or up (from the table). After that he
+    puts a card on top of the table (deck.put())
     '''
     def __init__(self, deck, name):
         self.deck = deck
@@ -110,12 +154,19 @@ class Hand:
         for i in range(7):
             self.cards.append(self.deck.get_down())
 
-    def throw(self, position):
+    def throw(self, position, flip=False):
         if len(self.cards) != 8:
             raise IllegalMovement("Can't throw before getting")
 
+        if flip:
+            toeval = copy.copy(self.cards)
+            del toeval[position]
+            pos, val = self.evaluate(toeval)
+            # if val > 4:
+            #    raise IllegalMovement("Can't flip with more than 4")
+
         card = self.cards[position]
-        pinfo('*** {} Putting card in position {}: {}'.format(self.name,
+        pinfo('{} throwing card in position {}: {}'.format(self.name,
               position, card))
         del self.cards[position]
         self.deck.put(card)
@@ -124,21 +175,18 @@ class Hand:
         if len(self.cards) != 7:
             raise IllegalMovement("Can't get more than one card")
         card = self.deck.get_down()
-        pinfo('*** {} Getting card from deck: {}'.format(self.name, card))
+        pinfo('{} getting card from deck: {}'.format(self.name, card))
         self.cards.append(card)
 
     def get_table(self):
         if len(self.cards) != 7:
             raise IllegalMovement("Can't get more than one card")
         card = self.deck.get_table()
-        pinfo('*** {} Getting card from table: {}'.format(self.name, card))
+        pinfo('{} getting card from table: {}'.format(self.name, card))
         self.cards.append(card)
 
     def value(self):
-        val = 0
-        for card in self.cards:
-            val += card.value
-        return val
+        return self.evaluate(self.cards)
 
     def __repr__(self):
         repstr = '{}: '.format(self.name)
@@ -146,18 +194,120 @@ class Hand:
             repstr += repr(card) + ' '
         return(repstr)
 
+    def evaluate_group(self, cards):
+        l = len(cards)
+        distance = 0
+        for i in range(l - 1):
+            distance += cards[i + 1].number - (cards[i] + 1)
+        return distance
+
+    def straight_evaluate(self, base):
+        l = len(base)
+        values = [0] * l
+        for n in range(l):
+            if n >= 2:
+                if ((base[n].number == (base[n - 1].number + 1)) and
+                        (base[n - 1].number == base[n - 2].number + 1) and
+                        (base[n].suit == base[n - 1].suit) and
+                        (base[n - 1].suit == base[n].suit)):
+                    values[n] = 0
+                    values[n - 1] = 0
+                    values[n - 2] = 0
+                elif ((base[n].number == base[n - 1].number) and
+                        (base[n - 1].number == base[n - 2].number)):
+                    values[n] = 0
+                    values[n - 1] = 0
+                    values[n - 2] = 0
+                else:
+                    values[n] = base[n].value
+            else:
+                values[n] = base[n].value
+        total = 0
+        for value in values:
+            total += value
+        positional = list(reversed(range(l)))
+        for n in range(l):
+            if values[n] == 0:
+                positional[n] = 0
+        total_pos = 0
+        for value in positional:
+            total_pos += value
+        return total, total_pos
+
+    def evaluate(self, base=None):
+        ''' Calculate all and best value of hand (lower is better) '''
+        if base is None:
+            base = self.cards
+        # Evaluate all possible permutations of the cards
+        # For any given combination, like 1O 2O 3O 9E 9O 9B 4E
+        # we will get some combination where the remaining card is
+        # at the end, so we would store the combination that brings
+        # that with the fewer remaining points
+        min_value = 900
+        min_pos = None
+        best_position = None
+        for position in itertools.permutations(base):
+            current_value, current_pos = self.straight_evaluate(position)
+            if current_value < min_value:
+                min_value = current_value
+                if min_pos is None:
+                    min_pos = current_pos
+                best_position = position
+            elif current_value == min_value:
+                if min_pos > current_pos:
+                    best_position = position
+                    min_pos = current_pos
+        return best_position, min_value
+
+        # print('Best position: {} value {}'.format(best_position, min_value))
+
+    def deck_or_table(self):
+        ''' Decide wether it is best to get the card from the deck or the
+            table '''
+        curpos, curval = self.value()
+        deck = self.deck.last()
+        toeval = copy.copy(self.cards)
+        toeval.append(deck)
+        pos, val = self.evaluate(toeval)
+        if val < curval:
+            return False         # Do table
+        return True              # Do deck
+
+    def move(self):
+        if self.deck_or_table():
+            self.get_deck()
+        else:
+            self.get_table()
+        pos, val = self.evaluate()
+        to_throw = pos[7]
+        nthrow = -1
+        for num, card in enumerate(self.cards):
+            if to_throw == card:
+                nthrow = num
+                break
+        if nthrow == -1:
+            raise Exception("WOT")
+        self.throw(nthrow)
+
+
+
 class Game(cmd.Cmd):
     prompt = '> '
 
     def __init__(self):
+        self.human_count = 0
+        self.machine_count = 0
         cmd.Cmd.__init__(self)
+        self.reset()
+        self._update_prompt()
+
+    def reset(self):
         self.deck = Deck()
         pinfo('Shuffling ...')
         pinfo('Giving cards ...')
         self.human = Hand(self.deck, 'human')
         self.machine = Hand(self.deck, 'machine')
         self.deck.first_up()
-        self._update_prompt()
 
     def _update_prompt(self):
         print(str(self.human) + ' top card: ' + str(self.deck.last()))
@@ -167,40 +317,111 @@ class Game(cmd.Cmd):
         return False
 
     def do_quit(self, args):
+        'Quit the game'
         print('Exiting ...')
         sys.exit(0)
 
     def do_debug(self, args):
+        'Show debugging info'
         pdebug('Debugging info: --- ')
         print(self.human)
-        print('Value ' + str(self.human.value()))
+        pos, val = self.human.value()
+        print('Best position {} value {}'.format(pos, val))
         print(self.machine)
-        print('Value ' + str(self.machine.value()))
+        pos, val = self.machine.value()
+        print('Best position {} value {}'.format(pos, val))
         print(self.deck)
         pdebug(' ------------------- ')
 
     def do_throw(self, args):
+        'Throw a card, face up to the table'
         try:
             position = int(args) - 1
             if position < 0 or position > 7:
                 raise ValueError
             self.human.throw(position)
+            self.machine.move()
         except ValueError:
             perror("Specify a position to throw from 1 to 8")
         except IllegalMovement as e:
             perror(str(e))
 
+    def do_flip(self, args):
+        'Throw a card face down (this finish the deal)'
+        try:
+            position = int(args) - 1
+            if position < 0 or position > 7:
+                raise ValueError
+            self.human.throw(position, flip=True)
+        except ValueError:
+            perror("Specify a position to flip from 1 to 8")
+        except IllegalMovement as e:
+            perror(str(e))
+        hpos, hval = self.human.value()
+        mpos, mval = self.machine.value()
+
+        if mval == 0:
+            mval = -10
+        if hval == 0:
+            hval = -10
+        print("Machine best position: {} Value {}".format(mpos, mval))
+        print("Human best position: {} Value {}".format(hpos, hval))
+
+        self.machine_count += mval
+        self.human_count += hval
+
+        print("Total points for human: {}".format(self.human_count))
+        print("Total points for machine: {}".format(self.machine_count))
+
+        if (self.human_count > 100):
+            print("Machine won")
+            self.machine_count = 0
+            self.human_count = 0
+        if (self.machine_count > 100):
+            print("Human won")
+            self.machine_count = 0
+            self.human_count = 0
+
+        self.reset()
+
     def do_deck(self, args):
+        'Get card from the deck'
         try:
             self.human.get_deck()
         except IllegalMovement as e:
             perror(str(e))
 
     def do_table(self, args):
+        'Get card from the table'
         try:
             self.human.get_table()
         except IllegalMovement as e:
             perror(str(e))
+
+    def do_move(self, args):
+        'Perform automatic movement'
+        self.human.move()
+        self.machine.move()
+
+    def do_evaluate(self, args):
+        '''Evaluate current position or a new one.
+           Example: evaluate 1O 2O 3O 5E 5O 5B 7E'''
+        if args == '':
+            # Evaluate current hand
+            pos, val = self.human.evaluate()
+            print("Best {} Value {}".format(pos, val))
+            return
+        base = []
+        for cardstr in args.split(' '):
+            try:
+                card = ParsedCard(cardstr)
+            except ParseError as e:
+                perror(str(e))
+                return
+            base.append(card)
+        # Evaluate given hand
+        pos, val = self.human.evaluate(base)
+        print("Best {} Value {}".format(pos, val))
 
     def default(self, args):
         perror('Unknown syntax: {}'.format(args))
